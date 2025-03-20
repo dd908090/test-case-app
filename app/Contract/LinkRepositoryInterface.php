5fa8c30e26a91b1c9e09cb5a06c7192990260dfc
dd908090 <dolgopol.dima@gmail.com>
2025-03-20 16:35:04 +0300

    Правки

--- app/Contract/LinkRepositoryInterface.php
+++ app/Contract/LinkRepositoryInterface.php
@@ -7,7 +7,7 @@ use app\Models\Link;
 interface LinkRepositoryInterface
 {
     public function getAll();
-    public function getById(Link $Link);
+    public function getById(int $linkId);
     public function delete(Link $Link);
     public function save(array $attributes);
     public function update(Link $Link, array $attributes);
